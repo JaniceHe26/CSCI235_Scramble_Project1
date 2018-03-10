0c8af0d57c66346b488583a0c8d6a30d06b128f9
Janice <Janice.He33@myhunter.cuny.edu>
2018-03-10 15:29:52 -0500

    Correct Guesses
    Logic

--- Game.cpp
+++ Game.cpp
@@ -13,11 +13,11 @@ using namespace std;
 void Game::run(string filePath) { //
     gameRunning = true;
 
-    bool fileExist = getLibrary(filePath);
+    bool fileExist = createLibrary(filePath);
     while (!fileExist) {  //While loop will keep going until user enter a correct file.
 //        cout << "File does not exist. Please enter a new file path: ";
 //        cin >> filePath;
-        fileExist = getLibrary(filePath);
+        fileExist = createLibrary(filePath);
     }
 //Postcondition:
 
@@ -39,6 +39,14 @@ void Game::run(string filePath) { //
     int numGuess = 3;
 //    int currentLevel = 0;
 //    int maxLevel = currentLevel;
+    int currentLength = 3;
+    string currentWord = getWord(currentLength);
+    string jumbledWord = jumble(currentWord);
+    string guessedWord; //a word user will enter
+    int stage = 1;
+
+
+
     while (gameRunning = true){
 
 
@@ -46,14 +54,46 @@ void Game::run(string filePath) { //
         cout<< "WORD JUMBLE BEGIN:"<<endl;
         cout<<"---------------------------------------------------------------------------------------------------------"<<endl;
         cout<<"Level:\t"<< level <<endl;//(put in keeping track of level function here)
+        cout<<"Stage:\t(" << stage << "/3)" << endl;
         cout<<"Score:\t"<< score <<endl;//(put in keeping track of score function here)
         cout<<"LifeLine Remaining:\t"<< lifeLineRem <<endl;//(Lifeline remaining function inside here)
         cout<<"---------------------------------------------------------------------------------------------------------"<<endl;
         cout<<"\n\n";
         cout<<"Number of guesses remaining:\t"<< numGuess <<endl; //NumberofTries in here
-        cout<<"Your word to unscramble:\t" <<endl;//put in the displayWord function here //cin>>
+        cout<<"Your word to unscramble:\t" << jumbledWord <<endl;//put in the displayWord function here //cin>>
+        cout<<"Please enter the word:\t";
 
-//      cout<<"Please enter the word:\t" cin>> <<endl;
+        cin>> guessedWord;
+
+        if (currentWord == guessedWord) {
+            cout << "Correct!" << endl;
+            stage++;
+            if(stage > 3){
+                stage = 1;
+                level++;
+                score = score + 10;
+                currentLength++;
+                if(currentLength > maxLength){
+                    //game is over******************************
+                }
+                while(library.count(currentLength) == 0 && currentLength <= maxLength){
+                    currentLength++;
+                }
+           }
+            score = score + 5;
+            currentWord = getWord(currentLength);
+            jumbledWord = jumble(currentWord);
+            numGuess = 3;
+
+
+
+        } else {
+            cout << "Incorrect!" << endl;
+            cout << "Stage:\t" << stage;
+            numGuess = numGuess - 1;
+            cout << "Number of Guess remaining for this stage:\t" << numGuess;
+        }
+
         cout<<"\n\n";
 
 
@@ -61,7 +101,6 @@ void Game::run(string filePath) { //
         cout<<"Game is over."<<endl;
 
 
-//This function will take in word of user. Must start with correct word guessed first
 
 
 
@@ -92,7 +131,7 @@ void Game::run(string filePath) { //
 }
 
 //This function will read file. Using a while loop just print out what is being read.
-bool Game::getLibrary(string filePath){
+bool Game::createLibrary(string filePath){
     ifstream file;
 //    file.exceptions ( ifstream::failbit | ifstream::badbit );
     try{
@@ -112,6 +151,9 @@ bool Game::getLibrary(string filePath){
 //This Function will store the word from file into a map of length and word. Also, check to see if word is existing.
 void Game::storeWord(string word) { //worry about length unique .use library
     auto length = (int) word.length(); //this will give length of the word by calling a function called length on <string>
+    if(length > maxLength){
+        maxLength = length;
+    }
     if(library.count(length) == 1){ //If key exist
         if (find(library[length].begin(), library[length].end(), word) == library[length].end()) { //Look through the element of vector
             library[length].push_back(word); //then add word
@@ -149,7 +191,6 @@ string Game::jumble(string word) {
     }
     return word;
 }
-
 
 
 
--- Game.h
+++ Game.h
@@ -29,15 +29,16 @@ private:
 //    string word = ""; //Word user will enter....should this be private?
 //    bool lifeLine = true;
     void level(int playerLvl);
-
     bool gameRunning = false;
+    int maxLength = 0;
     map <int, vector<string> > library;
-    bool getLibrary(string filePath);
+    bool createLibrary(string filePath);
     void storeWord(string word);
     string getWord(int length);
     string jumble(string word);
     string correctWord(int score); // keeps the correct word saved.
     void saveGame();
+
 
 //    void useLifeLine(); //extra life if player wish to use, using bool because if lifeline is activated (so thinking t it will activates)
 
